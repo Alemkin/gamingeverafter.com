cdd02fa9e2ac5991088572754e57c0d4ee49308e
alemkin@netchexonline.com <alemkin@netchexonline.com>
2019-01-24 19:20:46 -0500

    Misc changes

--- articles/index.js
+++ articles/index.js
@@ -1,19 +1,31 @@
 export var articles = [{
   title: 'Spider MAN',
   text: 'Peter Parker is all grown up, and we\'re here too say, yeah he\'s hot. Tune in for all of the risque snaps we took swinging around the skyscrpaers of Manhattan',
-  fileName: 'spiderman2018'
+  fileName: 'spiderman2018',
+  tags: [],
+  authors: [],
+  related: []
 }, {
   title: 'Mass Get Rekt',
   text: 'We can\'t tell you how many times we got rekt in this game. However, the upside is that commander femshep is a total badass, and we totally banged Tali',
-  fileName: 'massgetrekt'
+  fileName: 'massgetrekt',
+  tags: [],
+  authors: [],
+  related: []
 }, {
   title: 'Falling Out',
   text: 'Nothing like walking through the wasteland forever, never running out of things to do or find. Kind of odd that a post-apocalyptic wasteland is so full of life',
-  fileName: 'fallout4'
+  fileName: 'fallout4',
+  tags: [],
+  authors: [],
+  related: []
 }, {
   title: 'Kingdom of my Heart',
   text: 'A lifelong pursuit of fandom in everything disney, coupled with cheesy final fantasy characters and tropes? Yes please.',
-  fileName: 'kingdomhearts1'
+  fileName: 'kingdomhearts1',
+  tags: [],
+  authors: [],
+  related: []
 }]
 
 export default articles
