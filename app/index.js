de2711a50391cd398568fce6677232d4dc751ca7
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-02-01 09:50:19 -0500

    Removing use async ops

--- app/index.js
+++ app/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { registerPlugin } from 'use-async-ops'
-import { reduxPlugin } from 'use-async-ops-redux'
 
 import store from './store'
 import './service'
@@ -16,8 +14,6 @@ import '../fonts/DeterminationMonoWeb.woff'
 import { initTranslator } from './utils/translate'
 
 initTranslator()
-
-registerPlugin(reduxPlugin(store.dispatch))
 
 ReactDOM.render(
   <Provider store={store}>
--- app/store/middleware.js
+++ app/store/middleware.js
@@ -2,7 +2,6 @@ import { applyMiddleware } from 'redux'
 import invariant from 'redux-immutable-state-invariant'
 import { createLogger } from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
-import { enableMock, disableMock } from 'use-async-ops'
 
 export const sagaMiddleware = createSagaMiddleware()
 
@@ -17,8 +16,6 @@ export default () => {
       collapsed: true,
       diff: true
     }))
-    window.enableMock = enableMock
-    window.disableMock = disableMock
   }
 
   middleware.push(sagaMiddleware)
